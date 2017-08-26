f2fbe8c2f9c5e41b8faaea31d9761d5895f1f65d
Allen Shaw <xhui.xiao@gmail.com>
2017-08-26 23:43:34 +0800

    try callback in QML

--- qml/workerscript.js
+++ qml/workerscript.js
@@ -1,7 +1,9 @@
 WorkerScript.onMessage = function(msg) {
     var param = msg.params.get(0)
     console.log(param, param.dm, param.device)
-    var services = param.dm.connectToDevice(param.device)
+    var services = param.dm.connectToDevice(param.device)/*, function() {
+        console.log("callback called from C++")
+    })*/
     for (var i=0; i<services.length; i++) {
         console.log(services[i].uuid, services[i].name)
     }
